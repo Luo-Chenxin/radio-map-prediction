8abebcc0b47b72cd5e46b24c6a2bf2ae28defdd1
LUO Chenxin <bzdzmq@gmail.com>
2026-05-16 20:48:33 +0200

    feat: dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -5,16 +5,16 @@ from torch.utils.data import Dataset
 from torchvision.io import read_image, ImageReadMode
 
 class RadioMapDataset(Dataset):
-    def __init__(self, config, device):
+    def __init__(self, config):
         self.config = config
-        self.device = device
+        self.device = 'cpu'
         self.tensor_dtype = torch.float32
 
     def __len__(self):
         return self.config.maps_number*self.config.transmitters_number
 
     def __getitem__(self, idx):
-        map_idx = idx / self.config.transmitters_number
+        map_idx = idx // self.config.transmitters_number
         tx_idx = idx % self.config.transmitters_number
         
         buildings = self._load_buildings(map_idx)
@@ -24,39 +24,23 @@ class RadioMapDataset(Dataset):
         gain = self._apply_threshold(gain)
         
         inputs = [buildings, transmitters]
+        mask = None
 
         if self.config.sparse_IRT4_number > 0:
             mask, h_coords, w_coords = self._generate_mask()
             if self.config.samples_number > 0:
                 samples_gain = self._generate_samples(gain, self.config.sparse_IRT4_number, h_coords, w_coords)
+                inputs.append(samples_gain)
+        else:
+            if self.config.samples_number > 0:
+                samples_gain = self._generate_samples(gain)
+                inputs.append(samples_gain)
 
         if self.config.cars_input:
             cars = self._load_cars(map_idx)
             inputs.append(cars)
-
-        #--------------------------
-
-        if self.config.samples_number > 0:
-            samples = self._generate_random_samples(gain)
         
-        if self.mode == 'sparse_random':
-            # samples_img = self._generate_random_samples(gain_img)
-            inputs.append(samples_img)
-        elif self.mode == 'sparse_fixed':
-            samples_img, mask_img = self._generate_fixed_samples(gain_img)
-            # 注意: IRT4 模式通常返回 mask 作为辅助信息
-            # 这里可以将 mask 存入 gain_img 的某个属性，或直接作为第 3 个返回值
-            inputs.append(samples_img)
-        
-        inputs = np.stack(inputs, axis=2)
-        if self.config.get('transform'):
-            inputs = self.config['transform'](inputs)
-            gain_img = self.config['transform'](gain_img)
-        
-        if self.mode == 'sparse_fixed':
-            return inputs, gain_img, mask_img
-        else:
-            return inputs, gain_img
+        return (inputs, gain) if mask is None else (inputs, gain, mask)
 
     def _load_buildings(self, map_idx):
         path = Path(self.config.root_dir)
@@ -87,9 +71,12 @@ class RadioMapDataset(Dataset):
         path = Path(self.config.root_dir)
         name = f"{map_idx}_{tx_idx}.png"
 
-        pathDPM = path / Path(self.config.DPM_cars_dir) if self.config.cars_simulation else Path(self.config.DPM_dir) / name
-        pathIRT2 = path / Path(self.config.IRT2_cars_dir) if self.config.cars_simulation else Path(self.config.IRT2_dir) / name
-        pathIRT4 = path / Path(self.config.IRT4_cars_dir) if self.config.cars_simulation else Path(self.config.IRT4_dir) / name
+        pathDPM = Path(self.config.DPM_cars_dir) if self.config.cars_simulation else Path(self.config.DPM_dir)
+        pathDPM = path / pathDPM / name
+        pathIRT2 = Path(self.config.IRT2_cars_dir) if self.config.cars_simulation else Path(self.config.IRT2_dir)
+        pathIRT2 = path / pathIRT2 / name
+        pathIRT4 = Path(self.config.IRT4_cars_dir) if self.config.cars_simulation else Path(self.config.IRT4_dir)
+        pathIRT4 = path / pathIRT4 / name
 
         if self.config.sparse_IRT4_number > 0:
             return read_image(str(pathIRT4), ImageReadMode.GRAY).to(device=self.device, dtype=self.tensor_dtype) / 255.0
@@ -137,16 +124,3 @@ class RadioMapDataset(Dataset):
         samples_gain[selected_h_coords, selected_w_coords] = gain[selected_h_coords, selected_w_coords]
 
         return samples_gain
-
-   #-----------------------------------------------
-    def _generate_random_samples(self, gain_img):
-        # 对应 RadioUNet_s 中的随机采样逻辑
-        num_samples = np.random.randint(low=10, high=300)
-        # ... 生成逻辑
-        pass
-
-    def _generate_fixed_samples(self, gain_img):
-        # 对应 RadioUNet_s_sprseIRT4 中的固定采样逻辑
-        # 使用 map_idx 作为 seed 生成固定样本
-        # ... 生成逻辑
-        pass

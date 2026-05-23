f85d76e6772b5aa7b8936667cdcee8ceffc12599
LUO Chenxin <bzdzmq@gmail.com>
2026-05-23 18:50:19 +0200

    fix: change partition to 3090

--- job.sh
+++ job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=train_radio_unet_clean_DPM    # Job name
-#SBATCH --partition=P100                         # Requested compute partition
+#SBATCH --partition=3090                         # Requested compute partition
 #SBATCH --gres=gpu:1                             # Request 1 GPU
-#SBATCH --time=2:30:00                           # Maximum task runtime
+#SBATCH --time=1:00:00                           # Maximum task runtime
 #SBATCH --output=%x_%j.out                       # Standard output log (%x: job name | %j: job ID)
 #SBATCH --error=%x_%j.err                        # Error log
 #SBATCH --mail-type=BEGIN,END,FAIL

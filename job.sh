c53db537c12f2ffb03aec1552ab15fdd5ca73044
LUO Chenxin <bzdzmq@gmail.com>
2026-05-23 18:33:35 +0200

    fix: delete build_sif.sh; fix job.sh

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=train_radio_unet_clean_DPM    # Job name
 #SBATCH --partition=P100                         # Requested compute partition
 #SBATCH --gres=gpu:1                             # Request 1 GPU
-#SBATCH --time=1:00:00                           # Maximum task runtime
+#SBATCH --time=2:30:00                           # Maximum task runtime
 #SBATCH --output=%x_%j.out                       # Standard output log (%x: job name | %j: job ID)
 #SBATCH --error=%x_%j.err                        # Error log
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -17,5 +17,5 @@ module load apptainer
 # Training using Apptainer
 # --nv：Enable NVIDIA graphics card support
 # --bind：Mount the host machine's current directory (.) to the /workspace directory inside the container.
-apptainer exec --nv --bind .:/workspace pytorch.sif -C /workspace \
+apptainer exec --nv --bind .:/workspace --pwd /workspace rmp_env.sif \
     python train_radio_unet_clean_DPM.py

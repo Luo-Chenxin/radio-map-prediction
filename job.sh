8fb93c2262c03e6fe8279a37ddf0706546e5efbc
LUO Chenxin <bzdzmq@gmail.com>
2026-05-23 19:43:13 +0200

    fix: job time limitation

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=train_radio_unet_clean_DPM    # Job name
 #SBATCH --partition=3090                         # Requested compute partition
 #SBATCH --gres=gpu:1                             # Request 1 GPU
-#SBATCH --time=1:00:00                           # Maximum task runtime
+#SBATCH --time=06:00:00                           # Maximum task runtime
 #SBATCH --output=%x_%j.out                       # Standard output log (%x: job name | %j: job ID)
 #SBATCH --error=%x_%j.err                        # Error log
 #SBATCH --mail-type=BEGIN,END,FAIL

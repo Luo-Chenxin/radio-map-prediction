f5a01caaeb4162266752a42daa6ce8b653bbd321
LUO Chenxin <bzdzmq@gmail.com>
2026-05-24 12:49:57 +0200

    chore: rename job.sh to train_radio_unet.sh; add slum outlog into gitigore

--- train_radio_unet.sh
+++ train_radio_unet.sh
@@ -2,14 +2,17 @@
 #SBATCH --job-name=train_radio_unet_clean_DPM    # Job name
 #SBATCH --partition=3090                         # Requested compute partition
 #SBATCH --gres=gpu:1                             # Request 1 GPU
-#SBATCH --time=06:00:00                           # Maximum task runtime
-#SBATCH --output=%x_%j.out                       # Standard output log (%x: job name | %j: job ID)
-#SBATCH --error=%x_%j.err                        # Error log
+#SBATCH --time=03:40:00                          # Maximum task runtime
+#SBATCH --output=outputs/%x_%j.out               # Standard output log (%x: job name | %j: job ID)
+#SBATCH --error=outputs/%x_%j.err                # Error log
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=chenxin.luo@ip-paris.fr
 
 # Force the working directory to be the project root directory
 #SBATCH --chdir=/home/infres/cluo-25/radio-map-prediction 
+
+# Make outputs directory
+mkdir -p outputs
 
 # Load the Apptainer module
 module load apptainer
